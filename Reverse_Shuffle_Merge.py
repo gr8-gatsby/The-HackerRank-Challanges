3bc4c06ce40202b28e0191ca75ebcca68cfc9cfb
gr8-gatsby <94356605+gr8-gatsby@users.noreply.github.com>
2021-12-29 13:13:02 +0200

    Delete Reverse_Shuffle_Merge.py

--- Reverse_Shuffle_Merge.py
+++
@@ -1,9 +0,0 @@
-def maxMin(k, arr):
-    unfair = sys.maxsize + 1
-    arr = sorted(arr)
-
-    for i in range(len(arr) - k + 1):
-        if arr[i + k - 1] - arr[i] < unfair:
-            unfair = arr[i + k - 1] - arr[i]
-
-    return unfair
